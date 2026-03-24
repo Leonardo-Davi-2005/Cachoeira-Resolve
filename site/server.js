52dadc08cec1b57aa175c8594f435d19cb22acf4
TeteuzinBr <teteudavi7@gmail.com>
2026-03-24 20:50:28 -0300

    atulizacoes

--- site/server.js
+++ site/server.js
@@ -77,6 +77,8 @@ app.post("/salvar", (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta " + PORT)
 })
